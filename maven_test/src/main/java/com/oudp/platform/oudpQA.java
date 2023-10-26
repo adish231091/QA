31552bc2c64aa754df4a7082a81949083784f41a
adish231091 <adish.231091@gmail.com>
2023-10-26 16:35:41 +0530

    Adding another suite

---
+++ maven_test/src/main/java/com/oudp/platform/oudpQA.java
@@ -0,0 +1,21 @@
+package com.oudp.platform;
+
+public class oudpQA
+{
+    public static boolean FirstName(String str1)
+    {
+        String fname = str1;
+        if (fname.isEmpty())
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static void main(String args[]) {
+
+    }
+
+}
---
+++ maven_test/src/test/java/com/oudp/platform/test_oudpQA.java
@@ -0,0 +1,13 @@
+package com.oudp.platform;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+public class test_oudpQA {
+    @Test
+    public void TestOUDPDevFirstName()
+    {
+        System.out.println("Checking FirstName is empty Or Not !!!!");
+        boolean var= false;
+        assertEquals(var,oudpDev.FirstName("   asdsadasd"));
+    }
+
+}
